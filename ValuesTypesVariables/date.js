1a96705ae403d79280bf0a412dcad2cbde6562f3
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-09-08 22:10:50 +0300

    Date object string rep methods

--- ValuesTypesVariables/date.js
+++ ValuesTypesVariables/date.js
@@ -14,3 +14,8 @@ console.log('The date of the month: ', later.getDate()); // => 5: day of the wee
 console.log('Day of the week: ', later.getDay()); // => 5: day of the week 0 is sunday 5 is friday
 console.log('Get the hours: ', later.getHours()); // => 17: 5pm local time
 console.log('get UTC hours: ', later.getUTCHours());// hpurs in UTC time; depends on timezone
+console.log('Date to string: ', later.toString()); // => returns a string rep on the date
+console.log('Date to UTC string: ', later.toUTCString()); // => returns UTC string representation
+console.log('Date to local Date string: ', later.toLocaleDateString()); // => Local date string
+console.log('Date to local Time string: ', later.toLocaleTimeString()); // => local time string
+console.log('ISO date string: ', later.toISOString()); // => ISO Date String representation
