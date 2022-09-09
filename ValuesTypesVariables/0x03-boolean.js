7b8b68e6f5313b41991a866b33caddf1e87e1d97
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-09-09 09:07:52 +0300

    boolean js

---
+++ ValuesTypesVariables/0x03-boolean.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+ * Simple examples showing how to use booleans
+ * Examples of booleans &&, || ==
+ */
+
+var a  = 3 // initializes a with value 3
+console.log('Test if the value of variable a equates to 3: ', (a == 3)); // evaluates to true if a = 3
