7e98782f3daf07b323c4a9d79d124760fda93f87
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-09-09 11:40:40 +0300

    copy objects

--- ValuesTypesVariables/0x05-copy_objects.js
+++ ValuesTypesVariables/0x05-copy_objects.js
@@ -10,3 +10,18 @@ for (var i = 0; i < a.length; i++) {
   b[i] = a[i]; // copy value to the given index to b[i]
 }
 console.log('Print the new array b:', b);
+
+/*
+ * function to compare to distinct objects i.e arrays
+ * uses for loop
+ * checks if the length of the arrays are similar
+ * checks if the elements are identical
+ */
+
+function equalArrays (a, b) {
+  if ( a.length != b.length) return false;
+  for (var i = 0; i < a.length; i++)
+    if (a[i] !== b[i]) return false;
+  return true;
+}
+console.log('Compare two arrays a and b:', equalArrays(a, b)); // compares array a and b and prints true/false 
