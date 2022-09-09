f46b4ccce1079f420455f712f09b9217615f1fbe
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-09-09 11:32:42 +0300

    quick fix

--- ValuesTypesVariables/0x05-copy_objects.js
+++ ValuesTypesVariables/0x05-copy_objects.js
@@ -7,6 +7,6 @@
 var a = ['a', 'b', 'c', 'd', 'e', 'f']; // An array we want to copy
 var b = []; // distinct array we want to copy into
 for (var i = 0; i < a.length; i++) {
-  b[i] = a[i]; copy value to the given index to b[i]
+  b[i] = a[i]; // copy value to the given index to b[i]
 }
 console.log('Print the new array b:', b);
