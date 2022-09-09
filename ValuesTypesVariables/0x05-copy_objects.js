a88da5a1ad5f624e3735f840f46758e2fc4e3d28
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-09-09 11:28:50 +0300

    copy objects

---
+++ ValuesTypesVariables/0x05-copy_objects.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * Program to copy the properties of one object or an array
+ * uses a for loop the iterate over the elements in the array
+ */
+
+var a = ['a', 'b', 'c', 'd', 'e', 'f']; // An array we want to copy
+var b = []; // distinct array we want to copy into
+for (var i = 0; i < a.length; i++) {
+  b[i] = a[i]; copy value to the given index to b[i]
+}
+console.log('Print the new array b:', b);
