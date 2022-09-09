64e369fba7c9efe28b8ea78e203bb517665b370b
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-09-09 09:11:44 +0300

    boolean js

--- ValuesTypesVariables/0x03-boolean.js
+++ ValuesTypesVariables/0x03-boolean.js
@@ -5,4 +5,11 @@
  */
 
 var a  = 3 // initializes a with value 3
+var b;
 console.log('Test if the value of variable a equates to 3: ', (a == 3)); // evaluates to true if a = 3
+
+if (a == 4)
+  b += 1;
+else
+  a += 1;
+console.log('Value of a after conditional statement', a);
